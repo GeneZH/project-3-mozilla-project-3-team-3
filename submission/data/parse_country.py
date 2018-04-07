81bd21feee6e520f123897e9ebafe49fac2b4cf3
YuhanL1n <yuhan.lin1994@gmail.com>
2018-04-06 18:22:27 -0600

    add id column

--- submission/data/parse_country.py
+++ submission/data/parse_country.py
@@ -4,11 +4,14 @@ data=pd.read_csv('20171013111831-SurveyExport.csv', encoding = "ISO-8859-1", dty
 country = 'Country or Region (optional)'
 country_ip = 'Country'
 tech_level = 'I consider myself:'
+id = 'Response ID'
+
 level = ['Ultra Nerd', 'Technically Savvy', 'Average User', 'Luddite']
 
 #fill missing data
 data_country = data[country].fillna(data[country_ip])
 data_tech = data[tech_level]
+data_id = data[id]
 for i, value in data_tech.iteritems():
     if pd.isnull(value):
         continue
@@ -16,5 +19,5 @@ for i, value in data_tech.iteritems():
         if l in value:
             data_tech[i] = l
 
-output = pd.concat([data_country, data_tech], axis=1)
+output = pd.concat([data_id, data_country, data_tech], axis=1)
 output.to_csv("country.csv", index=False)
